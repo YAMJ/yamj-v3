18f6b8ed4a0b355fa41b1ce32c934c666503e2bf
Stuart Boston <omertron@gmail.com>
2013-03-28 16:04:11 +0000

    Error trapping for sending files to core server

--- yamj3-filescanner/src/main/java/com/moviejukebox/filescanner/ScannerManagementImpl.java
+++ yamj3-filescanner/src/main/java/com/moviejukebox/filescanner/ScannerManagementImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.remoting.RemoteAccessException;
 import org.springframework.remoting.RemoteConnectFailureException;
 
 public class ScannerManagementImpl implements ScannerManagement {
@@ -87,8 +88,8 @@ public class ScannerManagementImpl implements ScannerManagement {
             return EXIT_CONNECT_FAILURE;
         }
 
+        FileImportDTO dto = null;
         try {
-            FileImportDTO dto;
             for (File file : fileList) {
                 dto = new FileImportDTO();
                 dto.setScanPath(directoryScanned.getAbsolutePath());
@@ -102,7 +103,12 @@ public class ScannerManagementImpl implements ScannerManagement {
                 }
 
                 LOG.info("{}Sending '{}' to the server...", LOG_MESSAGE, file.getName());
-                fileImportService.importFile(dto);
+                try {
+                    fileImportService.importFile(dto);
+                } catch (RemoteAccessException ex) {
+                    LOG.error("{}Failed to send object to the core server: {}", LOG_MESSAGE, ex.getMessage());
+                    LOG.error("{}{}", LOG_MESSAGE, (dto == null ? "No object found" : dto.toString()));
+                }
             }
         } catch (RemoteConnectFailureException ex) {
             LOG.error("{}Failed to connect to the core server: {}", LOG_MESSAGE, ex.getMessage());
