50dfbd90562b4136dad4aa06849ea67c7fec454c
Stuart Boston <>
2013-04-04 22:18:23 +0100

    Updated StageDirectoryDTO

--- yamj3-common/src/main/java/com/moviejukebox/common/dto/StageDirectoryDTO.java
+++ yamj3-common/src/main/java/com/moviejukebox/common/dto/StageDirectoryDTO.java
@@ -10,9 +10,9 @@ import java.util.List;
 public class StageDirectoryDTO implements Serializable {
 
     private static final long serialVersionUID = 4310308645268137615L;
-
     private String path;
     private long date;
+    private List<StageDirectoryDTO> stageDirs = new ArrayList<StageDirectoryDTO>(0);
     private List<StageFileDTO> stageFiles = new ArrayList<StageFileDTO>(0);
 
     public String getPath() {
@@ -42,9 +42,21 @@ public class StageDirectoryDTO implements Serializable {
     public void addStageFile(StageFileDTO stageFile) {
         this.stageFiles.add(stageFile);
     }
-    
+
+    public List<StageDirectoryDTO> getStageDirs() {
+        return stageDirs;
+    }
+
+    public void setStageDirs(List<StageDirectoryDTO> stageDirs) {
+        this.stageDirs = stageDirs;
+    }
+
+    public void addStageDir(StageDirectoryDTO stageDir) {
+        this.stageDirs.add(stageDir);
+    }
+
     @Override
     public String toString() {
-        return "StageDirectoryDTO{path='" + path + "', date=" + date + ", files=" + stageFiles.size() + "}";
+        return "StageDirectoryDTO{" + "path=" + path + ", date=" + date + ", stageDirs=" + stageDirs.size() + ", stageFiles=" + stageFiles.size() + '}';
     }
 }
