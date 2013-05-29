74cbb9deebf901dfddeff79365706ae14503e2b9
Stuart Boston <Stuart.Boston@Gmail.com>
2013-05-29 21:02:56 +0100

    Added build information to Manifest and main method

--- yamj3-filescanner/src/main/java/com/yamj/filescanner/ApplicationContextProvider.java
+++ yamj3-filescanner/src/main/java/com/yamj/filescanner/ApplicationContextProvider.java
@@ -8,13 +8,44 @@ public class ApplicationContextProvider implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext = null;
 
-    public static ApplicationContext getApplicationContext() {
-        return applicationContext;
-    }
-
+    /**
+     * Created during the initialisation of Spring
+     *
+     * @param applicationContext
+     * @throws BeansException
+     */
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         // Assign the ApplicationContext into a static variable
         this.applicationContext = applicationContext;
+    }
+
+    /**
+     * Get the application context
+     *
+     * @return
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * Check that the application context is configured
+     * @return
+     */
+    public static boolean isConfigured() {
+        return (applicationContext != null);
+    }
+
+    /**
+     * Get a bean using it's class and name
+     *
+     * @param <T>
+     * @param T
+     * @param beanName
+     * @return
+     */
+    public static <T> T getBean(Class T, String beanName) throws BeansException {
+        return (T) applicationContext.getBean(beanName);
     }
 }
