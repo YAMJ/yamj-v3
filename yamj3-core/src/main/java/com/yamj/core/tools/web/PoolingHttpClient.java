9c988bec1a4febf368117e00289bd77c922d27a8
modmax <modmax@abwesend.de>
2013-05-31 13:59:38 +0200

    Updated pooling http client

--- yamj3-core/src/main/java/com/yamj/core/tools/web/PoolingHttpClient.java
+++ yamj3-core/src/main/java/com/yamj/core/tools/web/PoolingHttpClient.java
@@ -1,11 +1,8 @@
 package com.yamj.core.tools.web;
 
-import com.moviejukebox.api.common.http.AbstractPoolingHttpClient;
 import com.yamj.common.tools.PropertyTools;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,14 +23,13 @@ import org.apache.http.params.HttpParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
+import org.yamj.api.common.http.DefaultPoolingHttpClient;
 
-public class PoolingHttpClient extends AbstractPoolingHttpClient implements DisposableBean {
+public class PoolingHttpClient extends DefaultPoolingHttpClient implements DisposableBean {
 
     private static final Logger LOG = LoggerFactory.getLogger(PoolingHttpClient.class);
     private final Map<String, Integer> groupLimits = new HashMap<String, Integer>();
     private final List<String> routedHosts = new ArrayList<String>();
-    private int connectionsMaxPerRoute = 1;
-    private int connectionsMaxTotal = 20;
 
     public PoolingHttpClient() {
         this(null, null);
@@ -69,82 +65,12 @@ public class PoolingHttpClient extends AbstractPoolingHttpClient implements Disp
         }
     }
 
-    public int getConnectionsMaxPerRoute() {
-        return connectionsMaxPerRoute;
-    }
-
-    @Override
-    public void setConnectionsMaxPerRoute(int connectionsMaxPerRoute) {
-        this.connectionsMaxPerRoute = connectionsMaxPerRoute;
-    }
-
-    public int getConnectionsMaxTotal() {
-        return connectionsMaxTotal;
-    }
-
-    @Override
-    public void setConnectionsMaxTotal(int connectionsMaxTotal) {
-        this.connectionsMaxTotal = connectionsMaxTotal;
-    }
-
-    @Override
-    protected ClientConnectionManager createClientConnectionManager() {
-        PoolingClientConnectionManager clientManager = new PoolingClientConnectionManager();
-        clientManager.setDefaultMaxPerRoute(connectionsMaxPerRoute);
-        clientManager.setMaxTotal(connectionsMaxTotal);
-        return clientManager;
-    }
-
     @Override
     public void destroy() throws Exception {
         ClientConnectionManager clientManager = super.getConnectionManager();
         if (clientManager != null) {
             clientManager.shutdown();
         }
-    }
-
-    @Override
-    public String requestContent(URL url) throws IOException {
-        return this.requestContent(url, null);
-    }
-
-    @Override
-    public String requestContent(URL url, Charset charset) throws IOException {
-        URI uri;
-        try {
-            uri = url.toURI();
-        } catch (URISyntaxException ex) {
-            throw new IllegalArgumentException("Invalid url " + url, ex);
-        }
-
-        return this.requestContent(uri, charset);
-    }
-
-    @Override
-    public String requestContent(String uri) throws IOException {
-        return this.requestContent(uri, null);
-    }
-
-    @Override
-    public String requestContent(String uri, Charset charset) throws IOException {
-        HttpGet httpGet = new HttpGet(uri);
-        return this.requestContent(httpGet, charset);
-    }
-
-    @Override
-    public String requestContent(URI uri) throws IOException {
-        return this.requestContent(uri, null);
-    }
-
-    @Override
-    public String requestContent(URI uri, Charset charset) throws IOException {
-        HttpGet httpGet = new HttpGet(uri);
-        return this.requestContent(httpGet, charset);
-    }
-
-    @Override
-    public String requestContent(HttpGet httpGet) throws IOException {
-        return this.requestContent(httpGet, null);
     }
 
     @Override
