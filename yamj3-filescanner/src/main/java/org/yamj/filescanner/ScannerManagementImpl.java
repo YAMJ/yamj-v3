dbaef660996aa4ec76e51f7b18e220651a1963ab
Stuart Boston <Stuart.Boston@Gmail.com>
2013-06-04 19:32:22 +0100

    Added Copyright message to File Scanner

--- yamj3-filescanner/src/main/java/org/yamj/filescanner/ScannerManagementImpl.java
+++ yamj3-filescanner/src/main/java/org/yamj/filescanner/ScannerManagementImpl.java
@@ -1,3 +1,25 @@
+/*
+ *      Copyright (c) 2004-2013 YAMJ Members
+ *      https://github.com/organizations/YAMJ/teams
+ *
+ *      This file is part of the Yet Another Media Jukebox (YAMJ).
+ *
+ *      The YAMJ is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      YAMJ is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with the YAMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Web: https://github.com/YAMJ/yamj-v3
+ *
+ */
 package org.yamj.filescanner;
 
 import org.yamj.common.cmdline.CmdLineParser;
@@ -73,6 +95,7 @@ public class ScannerManagementImpl implements ScannerManagement {
     private static final int MAX_INSTALL_AGE = PropertyTools.getIntProperty("filescanner.installation.maxdays", 1);
     // Map of filenames & extensions that cause scanning of a directory to stop or a filename to be ignored
     private static final Map<String, List<String>> DIR_EXCLUSIONS = new HashMap<String, List<String>>();
+    private static final List<String> DIR_IGNORE_FILES = new ArrayList<String>();
 
     static {
         // Set up the break scanning list. A "null" for the list means all files.
