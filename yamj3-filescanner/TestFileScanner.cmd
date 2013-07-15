79c82f9c02ed54c3cecca444c075165211cd61bb
Stuart Boston <Stuart.Boston@Gmail.com>
2013-07-15 15:32:59 +0100

    Remove unneeded files

--- yamj3-filescanner/TestFileScanner.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set TESTDIR=%CD%\testdir
-cd "%CD%\target\yamj3-filescanner-3.0-SNAPSHOT-bin"
-
-call filescanner -d "%TESTDIR%" -w false
-pause
--- yamj3-jetty/TestJetty.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set TESTDIR=%CD%
-cd "%CD%\target\yamj3-jetty-3.0-SNAPSHOT-bin"
-
-del lib\*.war
-copy ..\..\..\yamj3-core\target\*.war lib
-call jetty
-pause
