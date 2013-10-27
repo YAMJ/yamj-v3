0155d6aaf3b6b372bcdacc947cec4216715c467a
Stuart Boston <Stuart.Boston@Gmail.com>
2013-10-27 08:44:37 +0000

    Add Data Interfaces
    Credits
    Genres

---
+++ yamj3-core/src/main/java/org/yamj/core/database/model/IDataCredits.java
@@ -0,0 +1,40 @@
+/*
+ *      Copyright (c) 2004-2013 YAMJ Members
+ *      https://github.com/organizations/YAMJ/teams
+ *
+ *      This file is part of the Yet Another Media Jukebox (YAMJ).
+ *
+ *      The YAMJ is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      YAMJ is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with the YAMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Web: https://github.com/YAMJ/yamj-v3
+ *
+ */
+package org.yamj.core.database.model;
+
+import java.util.Set;
+import org.yamj.core.database.model.dto.CreditDTO;
+
+/**
+ * An interface used to designate that an entity has people associated with it
+ *
+ * @author Stuart
+ */
+public interface IDataCredits {
+
+    public Set<CreditDTO> getCreditDTOS();
+
+    public void addCreditDTO(CreditDTO creditDTO);
+
+    public void addCreditDTOS(Set<CreditDTO> creditDTOS);
+}
