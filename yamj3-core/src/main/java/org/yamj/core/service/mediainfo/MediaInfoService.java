e124a06800e62a7bf75275f59a88d3bfce5ba29b
modmax <modmax@abwesend.de>
2014-09-08 17:18:27 +0200

    Organized imports

--- yamj3-core/src/main/java/org/yamj/core/service/mediainfo/MediaInfoService.java
+++ yamj3-core/src/main/java/org/yamj/core/service/mediainfo/MediaInfoService.java
@@ -22,21 +22,10 @@
  */
 package org.yamj.core.service.mediainfo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
