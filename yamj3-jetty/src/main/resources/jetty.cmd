b73d60aedb474506ca5a32beec02c359e7dfd6ed
Stuart Boston <Stuart.Boston@Gmail.com>
2015-04-17 15:06:55 +0100

    Add filtered version and script files

---
+++ yamj3-jetty/src/main/resources/jetty.cmd
@@ -0,0 +1,7 @@
+@echo off
+REM ${project.name}
+REM ${project.description}
+REM Version: ${project.version}
+REM Git-SHA: ${git.commit.id}
+cls
+java -classpath .;lib/* org.yamj.jetty.Start %*
---
+++ yamj3-jetty/src/main/resources/jetty.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# ${project.name}
+# ${project.description}
+# Version: ${project.version}
+# Git-SHA: ${git.commit.id}
+clear
+java -classpath .:./lib/* org.yamj.jetty.Start $@
