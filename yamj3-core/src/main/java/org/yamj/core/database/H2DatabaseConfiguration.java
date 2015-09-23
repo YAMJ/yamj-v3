f6b3a825a8291365d51033f2e6caaea5c86dd5eb
mbader <mbader@RD-50019948-D.ads.mobilcom.de>
2015-09-23 14:44:20 +0200

    Added H2 spring profile for H2 embedded database

---
+++ yamj3-core/src/main/java/org/yamj/core/database/H2DatabaseConfiguration.java
@@ -0,0 +1,90 @@
+/*
+ *      Copyright (c) 2004-2015 YAMJ Members
+ *      https://github.com/organizations/YAMJ/teams
+ *
+ *      This file is part of the Yet Another Media Jukebox (YAMJ).
+ *
+ *      YAMJ is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      YAMJ is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with YAMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Web: https://github.com/YAMJ/yamj-v3
+ *
+ */
+package org.yamj.core.database;
+
+import static java.sql.Connection.TRANSACTION_READ_COMMITTED;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.yamj.core.hibernate.AuditInterceptor;
+
+@Configuration
+@EnableTransactionManagement
+@Profile("h2")
+public class H2DatabaseConfiguration extends AbstractDatabaseConfiguration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(H2DatabaseConfiguration.class);
+
+    @Override
+    @Bean
+    public DataSource dataSource() {
+        LOG.trace("Create new data source");
+
+        JdbcDataSource dataSource = new JdbcDataSource();
+        StringBuffer url = new StringBuffer().append("jdbc:h2:");
+        url.append(System.getProperty("yamj3.home", ".")).append("/database/yamj3;AUTO_SERVER=TRUE");
+        dataSource.setUrl(url.toString());
+        
+        dataSource.setUser("yamj3");
+        dataSource.setPassword("yamj3");
+        return dataSource;
+    }
+    
+    @Override
+    @Bean(destroyMethod="destroy")
+    public FactoryBean<SessionFactory> sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setEntityInterceptor(new AuditInterceptor());
+        sessionFactoryBean.setPackagesToScan("org.yamj.core.database.model");
+        
+        Properties props = new Properties();
+        props.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        props.put("hibernate.show_sql", showSql);
+        props.put("hibernate.generate_statistics", generateStatistics);
+        props.put("hibernate.hbm2ddl.auto", "update");
+        props.put("hibernate.connection.isolation", TRANSACTION_READ_COMMITTED);
+        props.put("hibernate.use_sql_comments", false);
+        props.put("hibernate.cache.use_query_cache", false);
+        props.put("hibernate.cache.use_second_level_cache", false);
+        props.put("hibernate.connection.CharSet", "utf8");
+        props.put("hibernate.connection.characterEncoding", "utf8");
+        props.put("hibernate.connection.useUnicode", false);
+        sessionFactoryBean.setHibernateProperties(props);
+        
+        return sessionFactoryBean;
+    }    
+}
+
