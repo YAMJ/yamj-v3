a8439f15d8f15a9ec6ef64977c755d7a06692f10
mbader <mbader@RD-50019948-D.ads.mobilcom.de>
2015-11-23 18:02:57 +0100

    Added stub for artwork sanity check task

---
+++ yamj3-core/src/main/java/org/yamj/core/service/tasks/ArtworkSanityTask.java
@@ -0,0 +1,58 @@
+/*
+ *      Copyright (c) 2004-2015 YAMJ Members
+ *      https://github.com/organizations/YAMJ/teams
+ *
+ *      This file is part of the Yet Another Media Jukebox (YAMJ).
+ *
+ *      YAMJ is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      YAMJ is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with YAMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Web: https://github.com/YAMJ/yamj-v3
+ *
+ */
+package org.yamj.core.service.tasks;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Task for checking if video, series or person is older than x days and marks
+ * those data entries as updated in order to force a rescan.
+ */
+@Component
+public class ArtworkSanityTask implements ITask {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RecheckTask.class);
+
+    @Autowired
+    private ExecutionTaskService executionTaskService;
+    
+    @Override
+    public String getTaskName() {
+        return "artworksanity";
+    }
+
+    @PostConstruct
+    public void init() {
+        executionTaskService.registerTask(this);
+    }
+
+    @Override
+    public void execute(String options) throws Exception {
+        // TODO
+    }
+}
