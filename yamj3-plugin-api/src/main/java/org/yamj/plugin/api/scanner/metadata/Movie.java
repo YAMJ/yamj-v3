90987c5be3469b180e8388e573186caa90937dad
mbader <mbader@RD-50019948-D.ads.mobilcom.de>
2016-04-13 12:51:54 +0200

    Firyt try of a plugin API for adding possibilities to add plugins to the core

---
+++ yamj3-plugin-api/src/main/java/org/yamj/plugin/api/scanner/metadata/Movie.java
@@ -0,0 +1,146 @@
+/*
+ *      Copyright (c) 2004-2015 YAMJ Members
+ *      https://github.com/organizations/YAMJ/teams
+ *
+ *      This file is part of the Yet Another Media Jukebox (YAMJ).
+ *
+ *      YAMJ is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      YAMJ is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with YAMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Web: https://github.com/YAMJ/yamj-v3
+ *
+ */
+package org.yamj.plugin.api.scanner.metadata;
+
+import java.util.*;
+
+public class Movie {
+
+    private Map<String, String> ids = new HashMap<>();
+    private String title;
+    private String originalTitle;
+    private int year;
+    private String plot;
+    private String outline;
+    private String tagline;
+    private String quote;
+    private String releaseCountryCode;
+    private Date releaseDate;
+    private int topRank = -1;
+    private int rating = -1;
+    private Collection<String> studios = new HashSet<>();
+
+    public Map<String, String> getIds() {
+        return ids;
+    }
+
+    public void setIds(Map<String, String> ids) {
+        this.ids = ids;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getOutline() {
+        return outline;
+    }
+
+    public void setOutline(String outline) {
+        this.outline = outline;
+    }
+
+    public String getTagline() {
+        return tagline;
+    }
+
+    public void setTagline(String tagline) {
+        this.tagline = tagline;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getReleaseCountryCode() {
+        return releaseCountryCode;
+    }
+
+    public void setReleaseCountryCode(String releaseCountryCode) {
+        this.releaseCountryCode = releaseCountryCode;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getTopRank() {
+        return topRank;
+    }
+
+    public void setTopRank(int topRank) {
+        this.topRank = topRank;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Collection<String> getStudios() {
+        return studios;
+    }
+
+    public void setStudios(Collection<String> studios) {
+        this.studios = studios;
+    }
+}
